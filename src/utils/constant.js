af011cdc39f40b7856824602d7e948963d49bf96
Javed hussain <140499890+javedhussain0@users.noreply.github.com>
2025-07-12 16:39:24 +0530

    Update constant.js

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,8 +1,5 @@
-const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+export const USER_API_END_POINT = "https://jobhunter-server-5tha.onrender.com/api/v1/user";
+export const JOB_API_END_POINT = "https://jobhunter-server-5tha.onrender.com/api/v1/job";
+export const APPLICATION_API_END_POINT = "https://jobhunter-server-5tha.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT = "https://jobhunter-server-5tha.onrender.com/api/v1/company";
 
-export const USER_API_END_POINT = `${BASE_URL}/api/v1/user`;
-export const JOB_API_END_POINT = `${BASE_URL}/api/v1/job`;
-export const APPLICATION_API_END_POINT = `${BASE_URL}/api/v1/application`;
-export const COMPANY_API_END_POINT = `${BASE_URL}/api/v1/company`;
-export const AUTH_API_END_POINT = `${BASE_URL}/api/v1/auth`;
-export const ADMIN_API_END_POINT = `${BASE_URL}/api/v1/admin`;
