6b508684269a60c0a77f710019177c6a088e6ff9
Javed hussain <140499890+javedhussain0@users.noreply.github.com>
2025-07-13 12:24:32 +0530

    Update constant.js

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -2,4 +2,3 @@ export const USER_API_END_POINT = "https://jobhunter-server-5tha.onrender.com/ap
 export const JOB_API_END_POINT = "https://jobhunter-server-5tha.onrender.com/api/v1/job";
 export const APPLICATION_API_END_POINT = "https://jobhunter-server-5tha.onrender.com/api/v1/application";
 export const COMPANY_API_END_POINT = "https://jobhunter-server-5tha.onrender.com/api/v1/company";
-
